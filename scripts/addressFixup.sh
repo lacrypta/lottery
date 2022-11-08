7932e42c793a99812a5a2ef6232be3a7c625cb38
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2022-11-08 17:47:28 -0300

    Add README to git in address fixup

--- scripts/addressFixup.sh
+++ scripts/addressFixup.sh
@@ -20,3 +20,4 @@ THE_SED="$(get_command_or_fail sed)"
 builtin cd "$("${THE_GIT}" rev-parse --show-toplevel || builtin true)" || exit 1
 
 "${THE_SED}" -i -r "s/https:\/\/polygonscan.com\/address\/0x[0-9a-fA-F]{40}/https:\/\/polygonscan.com\/address\/$("${THE_GREP}" '"address":' deployments/matic/Lottery.json | "${THE_HEAD}" -n1 | "${THE_CUT}" -d\" -f4)/g" README.md
+"${THE_GIT}" add README.md
